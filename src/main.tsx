55c84201c9619132f6b199c09ef41f7013eef10f
Firmansyah Yanuar <fyfirman@gmail.com>
2023-01-14 16:18:53 +0700

    feat: add msw setup and env example

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { env } from "./utils/env-variable";
+
+if (env.enableMock) {
+  require("~/mocks/setup");
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
